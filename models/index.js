7d79bcfd7eb19c4985b07ce245a7e635af55bb58
Chris-Warren <chriswarren_t@hotmail.com>
2022-11-04 14:16:10 +0000

    Added cheese data and added user data

--- models/index.js
+++ models/index.js
@@ -2,4 +2,12 @@ const User = require("./user.model")
 const Cheese = require("./cheese.model")
 const Board = require("./board.model")
 
+// one to many
+User.hasMany(Board)
+Board.belongsTo(User)
+
+// Many to many 
+Cheese.belongsToMany(Board,{through: 'Cheese_Board'})
+Board.belongsToMany(Cheese,{through: 'Cheese_Board'})
+
 module.exports = { User, Cheese, Board }
